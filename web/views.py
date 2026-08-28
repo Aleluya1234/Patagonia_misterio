c7ec5ed85b5bce39dde7ff524e25291e2e8b207a
BenjaminN. <benjaminmoreno0809@gmail.com>
2026-08-28 00:09:20 +0000

    ACTUALIZO TODO

--- web/views.py
+++ web/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-import json
-
-def inicio(request):
-    return render(request, "inicio.html")
-
-def nosotros(request):
-    return render(request, "nosotros.html")
-
-def contacto(request):
-    return render(request, "contacto.html")
+import json, os
+from django.conf import settings
 
 def catalogo(request):
-    with open("datos.json", encoding="utf-8") as f:
+    ruta_json = os.path.join(settings.BASE_DIR, "datos.json")
+    with open(ruta_json, encoding="utf-8") as f:
         misterios = json.load(f)
     return render(request, "catalogo.html", {"misterios": misterios})
+
+def inicio(request):
+    return render(request, "inicio.html", {"mensaje": "Explora los misterios de la Patagonia chilena"})
+
+def nosotros(request):
+    return render(request, "nosotros.html", {"equipo": "Grupo de estudiantes apasionados por la historia y las leyendas"})
+
+def contacto(request):
+    return render(request, "contacto.html", {"email": "contacto@patagonia-misterios.com"})
+
